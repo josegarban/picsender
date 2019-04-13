8f70d31bce4d928e22bce36596fca3aa27fb95f0
josegarban <36708774+josegarban@users.noreply.github.com>
2019-04-13 17:11:21 -0400

    Working on tests to create child table, under the present configuration they can be created with the exact same fields as the parent table.

--- tests.py
+++ tests.py
@@ -1,8 +1,13 @@
 import main
+import dbchild, dbhandler
 import xlstolist, xlsxtolist
 from pprint import pprint
 
 FILENAMES = ["test.xlsx", "test.xls"]
+
+####################################################################################################
+######################################## TEST LOADING DATA ######################################### 
+####################################################################################################
 
 def test_get_firstrow(filenames, input_sheetname = ""):
     print("Testing getting the first row in the test files with a sheet name.")
@@ -42,8 +47,45 @@ def test_get_otherrows(filenames, input_sheetname = ""):
             pprint(test)
     print("")
 
-
 #test_get_firstrow(FILENAMES, input_sheetname = "Feuille1")
 #test_get_otherrows(FILENAMES, input_sheetname = "Feuille1") 
  
-main.populate_db()
+#main.populate_db()
+
+####################################################################################################
+################################### TEST CHILDREN CREATION ######################################### 
+####################################################################################################
+
+def test_dbchild(sql_filename, sql_parenttable, sql_childtable):
+    
+    input_dict = dbhandler.get_alldbrows(sql_filename,
+                               sql_parenttable,
+                               "id",
+                               True)
+    
+    parent_columns = [x[0] for x in dbhandler.dictfieldnames_to_tuplist(input_dict)]
+    print("Columns", parent_columns)
+    
+    parent_keys = dbhandler.get_alldbkeys(sql_filename,
+                              sql_parenttable,
+                              "id",
+                              True)
+    
+    relation_tuplist = dbchild.get_relations(parent_columns)
+    
+    dbchild.create_childtable(input_dict,
+                              relation_tuplist,
+                              sql_filename,
+                              sql_parenttable,
+                              sql_childtable,
+                              True)
+            
+    return None
+
+####################################################################################################
+
+FILENAME = "output.sqlite"
+PARENTTABLE = "Processed_infofile"
+CHILDTABLE = "Child"
+
+test_dbchild(FILENAME, PARENTTABLE, CHILDTABLE)
